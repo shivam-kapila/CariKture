0b2161021112b8271733dfb318590e16416650b4
shivam-kapila <shivamkapila4@gmail.com>
2019-04-02 17:15:31 +0530

    Almost Done

---
+++ index.js
@@ -0,0 +1,7 @@
+// Can also be used with $(window).load(function() 
+$(document).ready(function () {
+    $('.flexslider').flexslider({
+        animation: "fade",
+        navigationText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>']
+    });
+});
